46be16a46d566eafdc8ca54d4b8ee46982c6dae7
Moch. Nurul Muaffan <muaffan92@gmail.com>
2021-07-16 10:26:06 +0700

    v1.3.2

---
+++ app/Controllers/Contact.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\TableModels;
+
+class Contact extends BaseController
+{
+    public function __construct()
+    {
+        // MODAL
+        $this->TableModels = new TableModels();
+    }
+    
+    public function index()
+    {
+        $layouts = [
+            'menu' => 'contact',
+            'header' => 'Contact Us',
+            'getMenu' => $this->TableModels->setData('menu', '*', '', '', '', '', ['menu' => 'ASC'])->getResultArray(),
+            'getKategoriMenu' => $this->TableModels->setData('kategori_menu', '*', '', '', '', '', '')->getResultArray(),
+            'getSosmed' => $this->TableModels->setData('sosmed', '*', '', '', '', '', '')->getResultArray(),
+            'Modals'=> $this->TableModels,
+        ];
+
+        $data = [
+            'getInfoHeader' => $this->TableModels->setData('info_header', '*', ['menu' => $layouts['menu']], '', '', '', '')->getRowArray(),
+            'getCabang' => $this->TableModels->setData('cabang', '*', '', '', '', '', '')->getResultArray(),
+            'getListTipe' => $this->TableModels->setData('list_tipe', '*', '', '', '', '', '')->getResultArray(),
+        ];
+
+        echo view('company_profile/Help/Layouts/header', $layouts);
+        echo view('company_profile/Help/Contact/index', $data);
+        echo view('company_profile/Help/Layouts/footer');
+    }
+}
--- app/Controllers/Help.php
+++ app/Controllers/Help.php
@@ -30,27 +30,5 @@ class Help extends BaseController
         echo view('company_profile/Help/index',$data);
         echo view('company_profile/Layouts/footer');
     }
-
-    public function contact()
-    {
-        $layouts = [
-            'menu' => 'contact',
-            'header' => 'Contact Us',
-            'getMenu' => $this->TableModels->setData('menu', '*', '', '', '', '', ['menu' => 'ASC'])->getResultArray(),
-            'getKategoriMenu' => $this->TableModels->setData('kategori_menu', '*', '', '', '', '', '')->getResultArray(),
-            'getSosmed' => $this->TableModels->setData('sosmed', '*', '', '', '', '', '')->getResultArray(),
-            'Modals'=> $this->TableModels,
-        ];
-
-        $data = [
-            'getInfoHeader' => $this->TableModels->setData('info_header', '*', ['menu' => $layouts['menu']], '', '', '', '')->getRowArray(),
-            'getCabang' => $this->TableModels->setData('cabang', '*', '', '', '', '', '')->getResultArray(),
-            'getListTipe' => $this->TableModels->setData('list_tipe', '*', '', '', '', '', '')->getResultArray(),
-        ];
-
-        echo view('company_profile/Help/Layouts/header', $layouts);
-        echo view('company_profile/Help/Contact/index', $data);
-        echo view('company_profile/Help/Layouts/footer');
-    }
 }
 ?>
